50e28dbff413643bdd42e3ee9447f233f3445011
Mohammad Ehsan <mdehsan57@gmail.com>
2026-08-11 11:40:13 +0530

    refactor: reorder routes for correct execution sequence

--- src/app.js
+++ src/app.js
@@ -2,12 +2,18 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use("/test", (req, res) => {
-  res.send("Hello World! This is a test route.");
+// sequence of routes matter, the first route that matches will be executed. No matter how many routes match, only the first one will be executed. So, if you have a route that matches a specific path, it should be defined before a more general route.
+
+app.get("/user", (req, res) => {
+  res.send({ firstName: "John", lastName: "Doe" });
 });
 
-app.use("/", (req, res) => {
-  res.send("Hello World! This is the default route.");
+app.post("/user", (req, res) => {
+  res.send({ message: "User created successfully!" });
+});
+
+app.use("/test", (req, res) => {
+  res.send("Hello World! This is a test route.");
 });
 
 app.listen(port, () => {
